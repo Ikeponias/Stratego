87bf48528d5703b87470e58dc8d51a4ae8a11742
ikeponias <sephiroth929@gmail.com>
2018-09-19 21:00:57 +0900

    first commit

---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Linq;
+
+public class Piece : MonoBehaviour {
+    public GameObject CanMoveArea;
+
+	// Use this for initialization
+	void Start () {
+        // iTween.MoveTo(gameObject, iTween.Hash("x", 1f));
+        // Destroy(gameObject, 3f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Move(PieceShadow pieceShadow)
+    {
+        CanMoveArea closestCanMoveArea = GameObject.FindGameObjectsWithTag("CanMoveArea")
+            .OrderBy(go => Vector3.Distance(go.transform.position, pieceShadow.transform.position))
+            .FirstOrDefault().GetComponent<CanMoveArea>();
+
+        int posX = closestCanMoveArea.PosX;
+        int posY = closestCanMoveArea.PosY;
+
+        iTween.MoveBy(this.gameObject, iTween.Hash("x", posX, "y", posY));
+
+        iTween.MoveTo(pieceShadow.gameObject, iTween.Hash("x", this.transform.position.x+posX, "y", this.transform.position.y+posY));
+
+    }
+}
---
+++ Assets/Scripts/PieceShadow.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Linq;
+
+public class PieceShadow : MonoBehaviour {
+
+    public GameObject ParentPiece;
+    public GameObject CanMoveArea;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        GameObject g = Instantiate(
+            CanMoveArea,
+            new Vector2(ParentPiece.transform.position.x, ParentPiece.transform.position.y),
+            Quaternion.identity
+        );
+
+        g.transform.parent = ParentPiece.transform;
+    }
+
+    void OnMouseDrag()
+    {
+        GameObject closestCanMoveArea = GameObject.FindGameObjectsWithTag("CanMoveArea")
+            .OrderBy(go => Vector3.Distance(go.transform.position, this.transform.position))
+            .FirstOrDefault();
+
+        GameObject[] notClosestCanMoveAreas = 
+            (GameObject [])GameObject.FindGameObjectsWithTag("CanMoveArea")
+            .Except(new GameObject[]{ closestCanMoveArea }).ToArray();
+
+
+        foreach(GameObject g in notClosestCanMoveAreas) {
+            g.GetComponent<MeshRenderer>().material.color = new Color(0, 0, 0);
+        }
+
+        MeshRenderer closestMeshRenderer = closestCanMoveArea.GetComponent<MeshRenderer>();
+        closestMeshRenderer.material.color = new Color(0, 0, 255);
+    }
+
+    void OnMouseUp()
+    {
+        Piece piece = ParentPiece.GetComponent<Piece>();
+        piece.Move(this);
+
+        GameObject[] canMoveAreas = GameObject.FindGameObjectsWithTag("CanMoveArea");
+        GameObject canMoveAreaParent = GameObject.FindGameObjectsWithTag("CanMoveAreaParent").FirstOrDefault();
+
+        foreach (GameObject canMoveArea in canMoveAreas)
+        {
+            Destroy(canMoveArea);
+        }
+
+        Destroy(canMoveAreaParent);
+    }
+}
